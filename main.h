d009fea56f7f81a7b20e14cd7cd80275dccc98e4
Nico35001 <Nico35001@users.noreply.github.com>
2013-12-01 15:57:39 +0100

    Update main.h

--- main.h
+++ main.h
@@ -4,9 +4,9 @@
 
 #define MAJOR_VERSION 1
 #define MINOR_VERSION 0            //Avant de changer de version, changer aussi le titre dans afficherInfos()!
-#define COMP_DAY 1               //Jour de la compilation
+#define COMP_DAY 1                //Jour de la compilation
 #define COMP_MONTH 12            //Mois de la compilation
-#define COMP_YEAR 2013          //Année de la compilation
+#define COMP_YEAR 2013          //Annï¿½e de la compilation
 
 using namespace std;
 void afficherInfos(char* chemin);
